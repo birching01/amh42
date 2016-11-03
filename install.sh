d591ee785a9230b527e8829596b7e197baadcad9
adminx <admin@DIY>
2016-11-03 14:40:21 +0800

    修复安装选项

--- install.sh
+++ install.sh
@@ -11,23 +11,22 @@ CopyrightLogo='
 ===================================================================================';
 echo "$CopyrightLogo";
 
-ConfirmInstall()
-{
-	echo "[Notice] Confirm Install AMH? please select: (1~4)"
-	select selected in 'Install AMH' 'Install AMH ++ （CN）' 'Install AMH ++ (USA)' 'Exit'; do break; done;
-	[ "$selected" == 'Exit' ] && echo 'Exit Install.' && exit;
 
-	if [ "$selected" == 'Install AMH 4.2' ]; then
-    wget http://amh.sh/file/AMH/4.2/amh.sh && chmod 775 amh.sh && ./amh.sh 2>&1 | tee amh.log;
-  elif [ "$selected" == 'Install AMH ++ （CN)' ]; then
-		wget https://raw.githubusercontent.com/birching01/amh42/master/amhcn.sh && chmod 775 amhcn.sh && ./amhcn.sh 2>&1 | tee amhcn.log;
-  elif [ "$selected" == 'Install AMH ++ (USA)' ]; then
-    wget https://raw.githubusercontent.com/birching01/amh42/master/amhusa.sh && chmod 775 amhusa.sh && ./amhusa.sh 2>&1 | tee amhusa.log;
+
+function ConfirmInstall()
+{
+    echo -e "[Notice] Confirm Install AMH? please select: (1~4)"
+    select selected in 'Install AMH' 'Install AMH ++ （CN）' 'Install AMH ++ (USA)' 'Exit'; do break; done;
+    [ "$selected" == 'Exit' ] && echo 'Exit Install.' && exit;
+    if [ "$selected" == 'Install AMH' ]; then
+   wget http://amh.sh/file/AMH/4.2/amh.sh && chmod 775 amh.sh && ./amh.sh 2>&1 | tee amh.log;
+    elif [ "$selected" == 'Install AMH ++ （CN）' ]; then
+	wget https://raw.githubusercontent.com/birching01/amh42/master/amhcn.sh && chmod 775 amhcn.sh && ./amhcn.sh 2>&1 | tee amhcn.log;
+    elif [ "$selected" == 'Install AMH ++ (USA)' ]; then
+	wget https://raw.githubusercontent.com/birching01/amh42/master/amhusa.sh && chmod 775 amhusa.sh && ./amhusa.sh 2>&1 | tee amhusa.log;
 	else
 		return;
-	fi;
-
+	fi;   
 	echo "[OK] You Selected: ${selected}";
 }
-
 ConfirmInstall;
