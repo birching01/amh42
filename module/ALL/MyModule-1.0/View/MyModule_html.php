e6f799d53de18a7b7374c97fa41226937528a2e2
adminx <admin@DIY>
2016-11-04 14:42:54 +0800

    更新于

---
+++ module/ALL/MyModule-1.0/View/MyModule_html.php
@@ -0,0 +1,6 @@
+<?php include('header.php'); ?>	<!-- 载入面板头部文件 -->
+<div id="body">
+	<h2>AMH » MyModule</h2>
+	<h1>Hello <?php echo $name;?>！</h1>  <!-- 输出$name变量值 -->
+</div>
+<?php include('footer.php'); ?>	<!-- 载入面板底部文件 -->
